08b5b134d39a8660fb6db723accb6934c68a5a6e
Adithyah Nair <adithyah.nair@gmail.com>
2022-05-10 16:45:29 +0530

    Added InspectionV3 ML Model.

---
+++ Recognize/Controller/ImageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recognize
+//
+//  Created by Adithyah Nair on 10/05/22.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+  
+    }
+
+
+}
+
