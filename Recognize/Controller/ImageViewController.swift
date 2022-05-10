7464089bae16cf11f5f867203bebebc1bb7badb5
Adithyah Nair <adithyah.nair@gmail.com>
2022-05-10 17:11:37 +0530

    Add functionality to pick image and display it on the app.

--- Recognize/Controller/ImageViewController.swift
+++ Recognize/Controller/ImageViewController.swift
@@ -7,14 +7,43 @@
 
 import UIKit
 
-class ImageViewController: UIViewController {
+class ImageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
-  
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        imagePicker.allowsEditing = true
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            
+            fatalError("Image picking failed.")
+            
+        }
+        
+        imageView.image = userPickedImage
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
 
+    }
+    
 }
 
